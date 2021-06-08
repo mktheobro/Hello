a71ac0b969b4ce09fc99f97458684c8057803ec6
mktheobro <theobrokuradoru@yahoo.co.jp>
2021-06-08 21:57:07 +0900

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by 久保政人 on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
